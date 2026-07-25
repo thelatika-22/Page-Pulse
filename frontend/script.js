05ca8af88b4045f33d8181424156316a3a9a8dc9
LATIKA SHARMA <latika2207@gmail.com>
2026-07-25 18:14:30 +0530

    Connect frontend to deployed backend

--- frontend/script.js
+++ frontend/script.js
@@ -14,7 +14,7 @@ analyzeBtn.addEventListener("click", async () => {
 
     try {
 
-        const API = "http://127.0.0.1:5000";
+        const API = "https://page-pulse-lmls.onrender.com";
        
 
         const response = await fetch(
